be7dbedac6732f67be46dab7452d75687cf21114
aa18afz <aa18afz@herts.ac.uk>
2025-10-26 22:09:04 +0000

    Refine comments in statistics_and_trends.py
    Updated comments for clarity and consistency in the code.

--- statistics_and_trends.py
+++ statistics_and_trends.py
@@ -16,7 +16,7 @@ def plot_relational_plot(df):
     fig, ax = plt.subplots(1, 1, dpi=1000)
     x = df.iloc[:, 0]
     y = df.iloc[:, 1]/1000000000
-    # Dividing by 100,000,000,000 to make the relational graph easier 
+    # Dividing by 100,000,000,000 to make the relational graph easier
     # to interpret
     ax.scatter(x, y)
     ax.set_title('Comapny Size vs Revenue')
@@ -30,8 +30,8 @@ def plot_relational_plot(df):
 
 def plot_categorical_plot(df):
     fig, ax = plt.subplots(dpi=1000)
-    counts = df.iloc[:, 2].value_counts() 
-    # Counting the repeating elements for pie chart 
+    counts = df.iloc[:, 2].value_counts()
+    # Counting the repeating elements for pie chart
     ax.pie(counts, autopct='%1.1f%%', labels=counts.index)
     ax.set_title('Seniority Distribution in Job Listings')
     plt.savefig('categorical_plot.png')
@@ -70,15 +70,15 @@ def preprocessing(df):
     # make use of quick features such as 'describe', 'head/tail' and 'corr'.
     df = df.loc[:, ['company_size', 'revenue', 'seniority_level']]
     # Using only the company_size, revenue and seniority level columns out
-    # of 13 columns in the data file for the analysis 
+    # of 13 columns in the data file for the analysis
 
     def clean_revenue_data(val):
-     # Due to data in columns being inconsistent, we clean the data
+        # Due to data in columns being inconsistent, we clean the data
         if pd.isna(val):  # if there is NA data
             return None  # we dont return anything in this if condition
         s = str(val).replace('€', '').replace(',', '').strip()
         # As data is written with currency symbols, notations and
-        # numerical values written in commas, we convert the data 
+        # numerical values written in commas, we convert the data
         # from str to float
         multiplier = 1
         if s.upper().endswith('B'):
@@ -88,7 +88,7 @@ def preprocessing(df):
             multiplier = 1e6  # Converting 'M' to million
             s = s[:-1]
         try:
-            return float(s) * multiplier 
+            return float(s) * multiplier
         except ValueError:  # If data is missing or in completely in
             return None     # invalid format, we dont return anything
 
